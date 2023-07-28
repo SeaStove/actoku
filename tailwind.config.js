858baf73edb4bbb2af58512178d7a4f30ba3fc89
jkalna22 <joshuakalna@yahoo.com>
2023-07-28 00:51:01 -0400

    Create a how to play page explaining the rules and restrictions #12

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
+  theme: { 
+    screens: {
+    'xxs': '280px',
+    // => @media (min-width: 280px) { ... }
+
+    'xs': '360px',
+    // => @media (min-width: 280px) { ... }
+
+    'sm': '600px',
+    // => @media (min-width: 600px) { ... }
+
+    'md': '768px',
+    // => @media (min-width: 768px) { ... }
+
+    'lg': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'xl': '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1550px',
+    // => @media (min-width: 1536px) { ... }
+  }},
   plugins: [],
 };
